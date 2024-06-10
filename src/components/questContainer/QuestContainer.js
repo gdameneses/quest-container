3d60664eff2311b3e409f3e46547cb69e0b31a1d
Gustavo Meneses <gdameneses@gmail.com>
2024-06-10 10:54:08 -0300

    fix: remove console.log statemenet

--- src/components/questContainer/QuestContainer.js
+++ src/components/questContainer/QuestContainer.js
@@ -22,6 +22,86 @@ const useFetchQuests = () => {
   return quests;
 };
 
+const dictionary = {
+  "install_extension": (quest) => ({
+    logo: `${process.env.PUBLIC_URL}/download.svg`,
+    title: `Install the CareerOS Chrome Extension`, 
+    text: 
+      { p: `
+        Unlock the superpowers of CareerOS with our Chrome Extension. This plugin lets you save companies, contacts, and jobs from Linkedin and other job boards. It also lets you access our messaging template and send Linkedin messages directly from CareerOS.
+        `
+      }
+  }),
+  "submit_application": (quest) => ({
+    logo: `${process.env.PUBLIC_URL}/send.svg`,
+    title: `Submit your application for ${quest.job.title} at ${quest.company.name}`,
+    text: {p: `Deadline: ${new Date(quest.due_date).toLocaleDateString("en-GB")}`}
+  }),
+  "resolve-comment": (quest) => ({
+    logo: `${process.env.PUBLIC_URL}/message.svg`, 
+    title: `Resolve a comment from [CA name]`,
+    text: { p: `You have an unresolved comment from your Career Advisor.`}
+  }),
+  "save_first_company": (quest) => ({
+    logo: `${process.env.PUBLIC_URL}/bookmark.svg`, 
+    title: `Save your first company`,
+    text: {
+      p: `Browse our company database for your favorite companies or new inspiration and save your first company to get started.`
+    }
+  }),
+  "add_contact": (quest) => ({
+    logo: `${quest?.company?.logo_url}`,
+    title: `Save a contact at ${quest?.company?.name}`, 
+    text: 
+      { p: `
+        You recently saved ${quest?.company?.name}. The next step is to now save a contact who’s working there to start networking with.
+        `
+      }
+  }),
+  "send_linkedin_message": (quest) => ({
+    logo: `${process.env.PUBLIC_URL}/avatar.svg`, // TODO: this should actually be an existing value from quest.contact. profile_image
+    title: `Send a LinkedIn message to ${quest?.contact?.first_name} ${quest?.contact?.last_name} at ${quest?.company?.name}`,
+    text: { 
+      format: `lineBreak`,
+      p: `Send a customized LinkedIn message to reach out to your new contact.`,
+      p2: `Don't worry! We'll help you with the wording.`
+    }
+  }),
+  "send_linkedin_connection": (quest) => ({
+    logo: `${process.env.PUBLIC_URL}/avatar.svg`, // TODO: this should actually be an existing value from quest.contact.profile_image,
+    title: `Send a LinkedIn Invitation to ${quest?.contact?.first_name} ${quest?.contact?.last_name} at ${quest?.company?.name}`,
+    text: { p: 'Send an invitation with a tailored message to your new contact. Our AI will help you!'}
+  }),
+  "send_first_email": (quest) => ({
+    logo: `${process.env.PUBLIC_URL}/avatar.svg`, // TODO: this should actually be an existing value from quest.contact.profile_image
+    title: `Send a first email to ${quest?.contact?.first_name} ${quest?.contact?.last_name} at ${quest?.company?.name}`,
+    text: { 
+      p: `Send a first email to ${quest?.contact?.first_name} ${quest?.contact?.last_name} at ${quest?.company?.name} to get a conversation started.`
+    }
+  }),
+  "add_new_contact": (quest) => ({
+    logo: `${quest?.company?.logo_url}`,
+    title: `Save a new contact at ${quest?.company?.name}`,
+    text: { p: `Add another contact to this company to start networking.` }
+  }),
+  "schedule_coffee_chat": (quest) => ({
+    logo: `${process.env.PUBLIC_URL}/coffee.svg`,
+    title: `Schedule a coffee with ${quest?.contact?.first_name} ${quest?.contact?.last_name} at ${quest?.company?.name}`,
+    text: { 
+      format: 'span',
+      p: `Get unique insights from your new contact by scheduling an informational interview with them. Make sure to use our`,
+      spanText: `coffee chat tips`,
+      p2:`to prepare for your meeting.`
+    }
+  }),
+  "send_follow_up_email": (quest) => ({
+    logo: `${process.env.PUBLIC_URL}/avatar.svg`, // TODO: this should actually be an existing value from quest.contact.profile_image
+    title: `Send a follow-up email to ${quest?.contact?.first_name} ${quest?.contact?.last_name} at ${quest?.company?.name}`,
+    text: { 
+      p: `Send a follow-up email to ${quest?.contact?.first_name} ${quest?.contact?.last_name} at ${quest?.company?.name} to get a conversation started.`}
+  })
+};
+
 /**
  * 
  * @param {QuestData} quest - An object containing quest data.
@@ -33,86 +113,9 @@ const getQuestProps = (quest) => {
    */
   
   // TODO: Change this implementation to a switch case as all objects are being created for every iteration
-  const dictionary = {
-    "install_extension": {
-      logo: `${process.env.PUBLIC_URL}/download.svg`,
-      title: `Install the CareerOS Chrome Extension`, 
-      text: 
-        { p: `
-          Unlock the superpowers of CareerOS with our Chrome Extension. This plugin lets you save companies, contacts, and jobs from Linkedin and other job boards. It also lets you access our messaging template and send Linkedin messages directly from CareerOS.
-          `
-        }
-    },
-    "submit_application": {
-      logo: `${process.env.PUBLIC_URL}/send.svg`,
-      title: `Submit your application for ${quest?.job?.title} at ${quest?.company?.name}`, text: {p: `Deadline: ${new Date(quest?.due_date).toLocaleDateString("en-GB")}`}
-    },
-    "resolve-comment": {
-      logo: `${process.env.PUBLIC_URL}/message.svg`, 
-      title: `Resolve a comment from [CA name]`,
-      text: { p: `You have an unresolved comment from your Career Advisor.`}
-    },
-    "save_first_company": {
-      logo: `${process.env.PUBLIC_URL}/bookmark.svg`, 
-      title: `Save your first company`,
-      text: {
-        p: `Browse our company database for your favorite companies or new inspiration and save your first company to get started.`
-      }
-    },
-    "add_contact": {
-      logo: `${quest?.company?.logo_url}`,
-      title: `Save a contact at ${quest?.company?.name}`, 
-      text: 
-        { p: `
-          You recently saved ${quest?.company?.name}. The next step is to now save a contact who’s working there to start networking with.
-          `
-        }
-    },
-    "send_linkedin_message": {
-      logo: `${process.env.PUBLIC_URL}/avatar.svg`, // this should actually be an existing value from quest.contact. profile_image
-      title: `Send a LinkedIn message to ${quest?.contact?.first_name} ${quest?.contact?.last_name} at ${quest?.company?.name}`,
-      text: { 
-        format: `lineBreak`,
-        p: `Send a customized LinkedIn message to reach out to your new contact.`,
-        p2: `Don't worry! We'll help you with the wording.`
-      }
-    },
-    "send_linkedin_connection": {
-      logo: `${process.env.PUBLIC_URL}/avatar.svg`, // this should actually be an existing value from quest.contact.profile_image,
-      title: `Send a LinkedIn Invitation to ${quest?.contact?.first_name} ${quest?.contact?.last_name} at ${quest?.company?.name}`,
-      text: { p: 'Send an invitation with a tailored message to your new contact. Our AI will help you!'}
-    },
-    "send_first_email": {
-      logo: `${process.env.PUBLIC_URL}/avatar.svg`, // this should actually be an existing value from quest.contact.profile_image
-      title: `Send a first email to ${quest?.contact?.first_name} ${quest?.contact?.last_name} at ${quest?.company?.name}`,
-      text: { 
-        p: `Send a first email to ${quest?.contact?.first_name} ${quest?.contact?.last_name} at ${quest?.company?.name} to get a conversation started.`
-      }
-    },
-    "add_new_contact": {
-      logo: `${quest?.company?.logo_url}`,
-      title: `Save a new contact at ${quest?.company?.name}`,
-      text: { p: `Add another contact to this company to start networking.` }
-    },
-    "schedule_coffee_chat": {
-      logo: `${process.env.PUBLIC_URL}/coffee.svg`,
-      title: `Schedule a coffee with ${quest?.contact?.first_name} ${quest?.contact?.last_name} at ${quest?.company?.name}`,
-      text: { 
-        format: 'span',
-        p: `Get unique insights from your new contact by scheduling an informational interview with them. Make sure to use our`,
-        spanText: `coffee chat tips`,
-        p2:`to prepare for your meeting.`
-      }
-    },
-    "send_follow_up_email": {
-      logo: `${process.env.PUBLIC_URL}/avatar.svg`, // this should actually be an existing value from quest.contact.profile_image
-      title: `Send a follow-up email to ${quest?.contact?.first_name} ${quest?.contact?.last_name} at ${quest?.company?.name}`,
-      text: { 
-        p: `Send a follow-up email to ${quest?.contact?.first_name} ${quest?.contact?.last_name} at ${quest?.company?.name} to get a conversation started.`}
-    }
-  };
 
-  const questProps = dictionary[quest.type];
+
+  const questProps = dictionary[quest.type](quest);
 
   return questProps;
 };
