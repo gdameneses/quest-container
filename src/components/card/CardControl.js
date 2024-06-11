3ce453ceff6d0bf17130138ce6df4a7fc03baae4
Gustavo Meneses <gdameneses@gmail.com>
2024-06-10 23:22:56 -0300

    chore: add jsDocs

--- src/components/card/CardControl.js
+++ src/components/card/CardControl.js
@@ -1,3 +1,9 @@
+/**
+ * Renders a set of control buttons for a card.
+ *
+ * @param {object[]} controlProps - An array of objects representing the control buttons.
+ * each array element represents an icon to be displayed
+ */
 const CardControl = ({ controlProps }) => {
 
     const createElement = (element) => (
--- src/components/card/CardText.js
+++ src/components/card/CardText.js
@@ -1,3 +1,16 @@
+/**
+ * Renders text content for a card, optionally with line breaks or a span element.
+ *
+ * @param {object} textProps - An object containing text content and formatting options.
+ *   - `textProps.p` (string): The primary paragraph text.
+ *   - `textProps.p2` (string | undefined): Optional additional paragraph text displayed after a line break.
+ *   - `textProps.spanText` (string | undefined): Optional text to be wrapped in a `<span>` element.
+ *   - `textProps.format` (string | undefined): Optional, Controls how the text is displayed.
+ *      - "lineBreak" (optional): Inserts a line break between `textProps.p` and `textProps.p2`.
+*       - "span" (optional): Wraps `textProps.spanText` within a `<span>` element.
+ *
+ * @returns {JSX.Element} The JSX element representing the card text.
+ */
 const CardText = ({textProps}) => {
     if (textProps.format === 'lineBreak') {
         return (
